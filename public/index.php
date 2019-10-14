ba7a43a3a7c9fb41dcac284df062b9e13b2c6e35
Laurent Segura <laurent.roger.segura@gmail.com>
2019-10-14 12:05:36 +0200

    mise a jour

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../vendor/autoload.php';
+use App\Wcs\Hello;
+use HelloWorld\SayHello;
+
+echo SayHello::world();
+echo Hello::Talk();
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Wcs;
+
+class Hello {
+    public static function Talk (){
+        return 'Hello World';
+    }
+}
